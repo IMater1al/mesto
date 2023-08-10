b6c155a74b997e0da79abb4713eaf9dcebab2bc5
Karen Petrosian <karenpetrosyan109@yandex.ru>
2023-08-10 18:11:08 +0500

    feat: добавить изменение юзера и начальных карт

---
+++ src/scripts/Api.js
@@ -0,0 +1,52 @@
+export default class Api {
+  constructor(token, cohort) {
+    this._token = token;
+    this._cohort = cohort;
+  }
+
+  async getUserInfo() {
+    return await fetch(`https://mesto.nomoreparties.co/v1/${this._cohort}/users/me`, {
+      headers: {
+        authorization: this._token
+      }
+    }).then(res => {
+      if (res.ok) {
+        return res.json();
+      }
+      return Promise.reject(`Ошибка: ${res.status}`);
+    });
+  }
+
+  async getInitialCards() {
+    return await fetch(`https://mesto.nomoreparties.co/v1/${this._cohort}/cards `, {
+      headers: {
+        authorization: this._token
+      }
+    }).then(res => {
+      if (res.ok) {
+        return res.json();
+      }
+      return Promise.reject(`Ошибка: ${res.status}`);
+    });
+  }
+
+  async editProfileData(name, activity) {
+    return await fetch(`https://mesto.nomoreparties.co/v1/${this._cohort}/users/me`, {
+      method: 'PATCH',
+      headers: {
+        authorization: this._token,
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify({
+        name: name,
+        about: activity
+      })
+    }).then(res => {
+      if (res.ok) {
+        return res.json();
+      }
+
+      return Promise.reject(`Ошибка: ${res.status}`);
+    });
+  }
+}
